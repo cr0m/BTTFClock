06ae53a18d802e6379a8bf39173fbfe0d1843f7f
cr0m <cromwell.rob@gmail.com>
2022-04-13 09:26:47 -0400

    Update code.py

--- code.py
+++ code.py
@@ -26,10 +26,8 @@ yellow_display.brightness = .2
 red_display.brightness = .1
 
 white_display.print("----")
-
 yellow_display.print("----")
 red_display.print("----")
-red_display.print("----"")
 
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
@@ -63,7 +61,6 @@ def yearBlink(year, message):
     if(random_death != 2):
         return
     if (start_number == year):
-
 
         #  Chaos #1 - 1:3 chance not blink year
         random_die = random.randint(1, 2)
@@ -128,7 +125,6 @@ while True:
 
     def modeUpDown():  #  Counting down
         global cntUD
-
         global start_number
         global direction
 
@@ -141,16 +137,12 @@ while True:
         else:
             start_number = start_number + 1
 
-
-
         yearBlink(1939, "1939-1945 2nd World War1939-1945")
         yearBlink(1969, "1969-Man on the moon-1969")
         yearBlink(1983, "BIRTHDAY")
         yearBlink(2020, "* COVID19 * COVID19 * COVID19 * ")
         yearBlink(2022, "CURRENT YEAR 20022 CURRENT YEAR ")
         yearBlink(2222, "2222 TEST 2")
-
-
 
         if (start_number == 1401):
             direction = "up"
@@ -173,8 +165,6 @@ while True:
         cntUD = cntUD + 1
         time.sleep(.01)
 
-
-
     def modeRandom():  # Random years between 1800-2500
         global cntR
         random_number = random.randint(1800, 2500)
@@ -194,14 +184,9 @@ while True:
             else:
                 white_display.print(("%s" % (json_data[0]["just_time"])))
 
-#     test = json_data[0]["just_time"]
-#     print(test)
-#     print("%s" % (json_data[0]["just_time"]))
-
         cntR = cntR + 1
         random_time = random.randint(0, 1)
         time.sleep(random_time)
-
 
     # modeRandom()
     modeUpDown()
